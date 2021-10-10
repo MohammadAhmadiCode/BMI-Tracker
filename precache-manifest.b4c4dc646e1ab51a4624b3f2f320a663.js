7d2931b9eedeca8c9196622208b2286ec624670c
MohammadAhmadiCode <itsthemo1@gmail.com>
2021-10-10 22:22:57 +0330

    Updates

---
+++ precache-manifest.b4c4dc646e1ab51a4624b3f2f320a663.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "388c22b7f4ef56b62180ade7562782ef",
+    "url": "/bmi-calculator/index.html"
+  },
+  {
+    "revision": "44eea3692e217f286420",
+    "url": "/bmi-calculator/static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "fd287358a448ae300170",
+    "url": "/bmi-calculator/static/css/main.160e4576.chunk.css"
+  },
+  {
+    "revision": "44eea3692e217f286420",
+    "url": "/bmi-calculator/static/js/2.94b87c7b.chunk.js"
+  },
+  {
+    "revision": "fd287358a448ae300170",
+    "url": "/bmi-calculator/static/js/main.2887f64f.chunk.js"
+  },
+  {
+    "revision": "666fcd94090191ed86d5",
+    "url": "/bmi-calculator/static/js/runtime~main.1c230995.js"
+  }
+]);
